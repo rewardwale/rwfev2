7506a4684d5753cc9d570e789168aae5fdf6b0fc
lokeshnandan <lokeshnandan007@gmail.com>
2025-05-21 18:27:00 +0530

    adding notfication

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -8,6 +8,7 @@ import { Footer } from "@/components/layout";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { isUserLoggedIn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import PushNotifications from "@/components/PushNotification";
 
 export default function HomePage() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
@@ -29,6 +30,8 @@ export default function HomePage() {
   return (
     <div className="flex h-screen bg-background flex-col">
       <Header />
+
+      <PushNotifications />
 
       <div className="flex scrollbar-hide">
         <div>{!isMobile && <Sidebar />}</div>
